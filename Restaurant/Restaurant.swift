7fa67347a1b446a1bdd614287def0fe17103de97
Abuzar Manzoor <abuzarmanzoor@hotmail.com>
2018-08-19 17:51:10 +0500

    Initial Commit

---
+++ Restaurant/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Abuzar Manzoor on 7/6/2018.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    var phone = ""
+    var rating = ""
+    
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
